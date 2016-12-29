f7112e6b0879f843f89ddfbb0376737ce9c6d74f
陈欣聪 <chenxincong@shgbit.com>
2016-12-29 16:47:30 +0800

    获取通讯录信息

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ var getTimeStamp = function(){
     return moment.format("YYYYMMDDHHmmssSSS")
 };
 
+var requestCommon = function(options,callback){
+    return new Promise(function(resolve,reject){
+        return request(options, function (error, response, body) {
+            var hasCallback = 'function' === typeof callback
+            if(error) {
+                if(hasCallback)  return callback(error);
+                return reject(error);
+            }
+            if (!error && response.statusCode == 200) {
+                if(hasCallback) return callback(null,body);
+                return resolve(body)
+            }
+        })
+    })
+}
+
 GBUnityAPI.prototype.init = function(options){
     this.host = options.host;
     this.port = options.port;
@@ -19,31 +35,26 @@ GBUnityAPI.prototype.init = function(options){
 };
 
 GBUnityAPI.prototype.validateUser = function(sessionId,userId,callback){
-    return new Promise(function(resolve,reject){
         var timestamp = getTimeStamp();
         var publickey = this.appName + timestamp;
         var privateKey =  this.privateKey
         var token = gbunitytoken(publickey,privateKey);
         var url =  this.host+'/api/contact/user/validation'+'?appName='+this.appName+'&userId='+userId+'&sessionId='+sessionId+'&timeStamp='+timestamp+'&token='+token;
-        request({url:url,json:true,method:"GET"}, function (error, response, body) {
-            var hasCallback = 'function' === typeof callback
-            if(error) {
-                if(hasCallback)  return callback(error);
-                return reject(error);
-            }
-
-            if (!error && response.statusCode == 200) {
-                if(hasCallback) return callback(null,body);
-                return resolve(body)
-            }
-        })
+        var options = {url:url,json:true,method:"GET"};
+        return requestCommon(options,callback);
     })
 };
 GBUnityAPI.prototype.getUserInfo = function(){
 
 };
 GBUnityAPI.prototype.getAddressBook = function(){
-
+        var timestamp = getTimeStamp();
+        var publickey = this.appName + timestamp;
+        var privateKey = this.privateKey;
+        var token = gbunitytoken(publickey,privateKey);
+        var url = this.host+'/api/contact/infomation'+'?appName='+this.appName+'&timeStamp='+timestamp+"&token="+token;
+        var options = {url:url,json:true,method:"GET"};
+        return requestCommon(options,callback);
 };
 GBUnityAPI.prototype.getOrganizationsOfUser = function(){
 
